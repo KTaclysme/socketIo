07cbf9132451b02c0b5a895685aa87aac9f8c5ce
kevin.ta <kevin.ta@emeria.eu>
2024-09-03 09:38:30 +0200

    feat(): update

--- server.js
+++ server.js
@@ -1,26 +1,65 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
+const http = require("http");
+const {Server} = require('socket.io');
+
 
 const app = express();
 const server = http.createServer(app);
-const port = 3002;
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+const users = {};
+
+// page d'accueil
+app.get("/", (req, res) => {
+	// une page html :)
+	res.sendFile( __dirname + "/public" + "/index.html" );
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-    console.log(`message: ${msg}`);
-  });
+
+io.on('connection' , (socket) => {
+	console.log('Un utilisateur est connecté');
+
+	socket.on("send", (data) => { // message envoyer à partir d'un client
+		const {from, msg} = data;
+
+		console.log("Message recu de "+from)
+		console.log("Message :  "+msg)
+
+		// chercher si le message est destiner à qqu
+		// @mounir bonjour je suis la
+
+		if(msg.startsWith('@') ){
+			let name = msg.substring(1, msg.indexOf(" "))
+			io.to(users[name]).emit("recupererMsg", msg.substring(name.length+2));
+		}else{
+			// message et envoyer a tout le monde
+			console.log("Message envoyer à tout le monde")
+			io.emit("recupererMsg", msg);
+		}
+
+		console.log("\n")
+		//io.to(users[nom]).emit("recupererMsg", msg); // pour l'ensemble des utilisateurs connecter
+	});
+
+	socket.on("register", (username) => { // message envoyer à partir d'un client
+		users[username] = socket.id
+		console.log('Un utilisateur est connecter sous le nom de '+ username);
+	});
+
+	socket.on("disconnect" , () =>{
+		for(let username in users){
+			if(users[username] == socket.id){
+				delete users[username];
+				break;
+			}
+		}
+		console.log('Un utilisateur est déconnecté');
+	});
+
 });
 
-server.listen(port, () => {
-  console.log(`server running at port ${port}`);
-});
+// demmarer le server
+server.listen(3001, () => {
+	console.log("Serveur et bien lancer sur le port 3001")
+}) 
