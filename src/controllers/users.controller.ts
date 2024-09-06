247cd70faa8e3f1ba3afb03e74ad5bea158ecbba
kevin.ta <kevin.ta@emeria.eu>
2024-09-06 12:24:45 +0200

    feta(socket): puducu

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,28 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
+
 
 const prisma = new PrismaClient();
+
+export const getUserNameById = async (id: number): Promise<string | null> => {
+    const user = await prisma.user.findUnique({
+        where: { id },
+        select: { name: true }
+    });
+    return user ? user.name : null;
+};
+
+export const getUserByName = async (name: string): Promise<User | null> => {
+    return await prisma.user.findUnique({
+        where: { name },
+    });
+};
+
+export const updateUser = async (userId: number, sockerId: string): Promise<User | null> => {
+    return await prisma.user.update({
+        where: { id: userId }, // Identifier l'utilisateur par ID
+        data: { sockerId } // Mettre à jour le champ sockerId
+    });
+};
 
 export const getUserById = async (id: number) => {
     return await prisma.user.findUnique({
