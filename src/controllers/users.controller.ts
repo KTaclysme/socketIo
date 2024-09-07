9b9bfd6d89fe62360937eef3016f79204a883156
kevin.ta <kevin.ta@emeria.eu>
2024-09-07 12:35:27 +0200

    feat(src): friend feature

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -11,6 +11,74 @@ export const getUserNameById = async (id: number): Promise<string | null> => {
     return user ? user.name : null;
 };
 
+export const addFriend = async (userId: number, friendName: string) => {
+    const user = await prisma.user.findUnique({
+        where: { id: userId }
+    });
+
+    if (!user) {
+        throw new Error('Utilisateur non trouvé');
+    }
+
+    const friend = await prisma.user.findUnique({
+        where: { name: friendName }
+    });
+
+    if (!friend) {
+        throw new Error('Ami non trouvé');
+    }
+
+    const existingFriendship = await prisma.user.findFirst({
+        where: {
+            id: userId,
+            friends: {
+                some: { id: friend.id }
+            }
+        }
+    });
+
+    if (existingFriendship) {
+        throw new Error('Vous êtes déjà amis avec cet utilisateur');
+    }
+
+    await prisma.user.update({
+        where: { id: userId },
+        data: {
+            friends: {
+                connect: { id: friend.id }
+            }
+        }
+    });
+
+    await prisma.user.update({
+        where: { id: friend.id },
+        data: {
+            friends: {
+                connect: { id: userId }
+            }
+        }
+    });
+};
+
+export const getFriend = async (userId: number) => {
+    return await prisma.user.findUnique({
+        where: { id: userId },
+        include: { friends: true }
+    });
+};
+
+export const getAllFriends = async (userId: number) => {
+    return await prisma.user.findMany({
+        where: {
+            friendOf: {
+                some: {
+                    id: userId
+                }
+            }
+        }
+    });
+};
+
 export const getUserByName = async (name: string): Promise<User | null> => {
     return await prisma.user.findUnique({
         where: { name },
@@ -19,8 +87,8 @@ export const getUserByName = async (name: string): Promise<User | null> => {
 
 export const updateUser = async (userId: number, sockerId: string): Promise<User | null> => {
     return await prisma.user.update({
-        where: { id: userId }, // Identifier l'utilisateur par ID
-        data: { sockerId } // Mettre à jour le champ sockerId
+        where: { id: userId }, 
+        data: { sockerId } 
     });
 };
 
